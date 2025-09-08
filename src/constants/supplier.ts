86f9d82588e39cac059c175cf8b06ef4f6df7884
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2025-09-08 19:57:11 +0300

    new update

--- src/constants/supplier.ts
+++ src/constants/supplier.ts
@@ -1,13 +1,13 @@
 export const DEFAULT_SUPPLIER = {
-  name: "spark",
-  phone: "01000086797",
-  formattedPhone: "201000086797", // Phone number with country code
+  name: "Elaraby",
+  phone: "01008397114",
+  formattedPhone: "201008397114", // Phone number with country code
 } as const;
 
 export const CONTACT_PHONES = {
-  main: "01000086797",
-  formattedMain: "201024911062",
-  support: "201000086797",
-  formattedSupport: "201000086797",
-  whatsapp: "201000086797",
+  main: "01008397114",
+  formattedMain: "201008397114",
+  support: "201008397114",
+  formattedSupport: "201008397114",
+  whatsapp: "201008397114",
 } as const;
